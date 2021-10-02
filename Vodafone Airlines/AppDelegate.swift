0d329843bb9e15b2e6f8543392dabc772c208d11
Hassan Ashraf <hassan.ashraf@meddaro.com>
2021-10-02 03:06:47 +0200

    Project Initialization

---
+++ Vodafone Airlines/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Vodafone Airlines
+//
+//  Created by Hassan Ashraf on 02/10/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
